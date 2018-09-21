2b956e45672a38d4263d59a123ce8f92ada692b8
Esta <efspitta@gmail.com>
2018-09-21 12:00:53 -0500

    New commits

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
 const staticMiddleware = express.static('./public');
+const {db} = require('./models')
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
+// const Page = db.define('page',{
+//   title : Sequelize.STRING,
+//   slug : Sequelize.STRING,
+//   content: Sequelize.TEXT,
+//   status: Sequelize.BOOLEAN
+// })
+
+// const User = db.define('user',{
+//   name: Sequelize.STRING,
+//   email: Sequelize.STRING
+// })
 
 app.use(morgan('dev'));
 app.use(staticMiddleware);
@@ -14,5 +30,12 @@ app.get('/', (req, res) => {
   res.send(`<h1> Hello World </h1>`);
 })
 
+async function synchronous (){
+  // await Page.sync()
+  // await User.sync()
+  await db.sync({force: true})
 
-app.listen(3000, () => console.log(`example appl listening on port 3000`));
+  app.listen(3000, () => console.log(`example appl listening on port 3000`));
+
+}
+synchronous()
