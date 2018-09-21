8ffdba7830aa2cf126de79ee80d9296b90de3cc8
Dejan Rajkovic <drajkovic992@gmail.com>
2018-09-21 10:38:39 -0500

    dejan

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+const staticMiddleware = express.static('./public');
+
+
+app.use(morgan('dev'));
+app.use(staticMiddleware);
+app.use(bodyParser.urlencoded({ extended: false }));
+console.log('Hello World');
+app.get('/', (req, res) => {
+  console.log('Hello World');
+  res.send(`<h1> Hello World </h1>`);
+})
+
+
+app.listen(3000, () => console.log(`example appl listening on port 3000`));
